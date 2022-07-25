00a746a9779f15bb19f44fba9137efa8fa878edc
Ronald <ronaldmirabal@outlook.es>
2022-07-25 15:49:56 +0000

    add

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -13,7 +13,7 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Personas') }}
+                                {{ __('Personas ') }}
                             </span>
 
                              <div class="float-right">
