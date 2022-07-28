4150da8b3a55557ffa9ef0bcc5c31c48c68a306d
Ronald <ronaldmirabal@outlook.es>
2022-07-28 14:36:25 +0000

    se mejoro el export del inventario a excel

--- app/Exports/InventoryExport.php
+++ app/Exports/InventoryExport.php
@@ -4,14 +4,50 @@ namespace App\Exports;
 
 use App\Models\Inventory;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class InventoryExport implements FromCollection
+class InventoryExport implements FromCollection, ShouldAutoSize, WithMapping, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Inventory::all();
+
+        return Inventory::with('people','area','brand','typeproduct')->get();
     }
+
+    public function map($inventory): array
+    {
+        return [
+            $inventory->id,
+            $inventory->stock,
+            $inventory->description,
+            $inventory->typeproduct->name,
+            $inventory->brand->name,
+            $inventory->model,
+            $inventory->serial,
+            $inventory->noplaca,
+            $inventory->people->first_name. " ".$inventory->people->last_name,
+            $inventory->area->name,
+        ];
+    }
+    public function headings(): array{
+        return [
+            '#Identificador',
+            'Cantidad',
+            'Descripción',
+            'Tipo de Articulo',
+            'Marca',
+            'Modelo',
+            'Serial',
+            'NoPlaca',
+            'Asignado',
+            'Área'
+
+        ];
+    }
+
 }
