f8a93d4e5c10134088271f944bafc1b6f624d32b
Ronald <ronaldmirabal@outlook.es>
2022-08-29 16:31:52 +0000

    se modifico el diseño pdf de label inventario

--- resources/views/inventory/printlabel.blade.php
+++ resources/views/inventory/printlabel.blade.php
@@ -6,26 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PrintLabel</title>
     <style type="text/css">
-.container {
-    background: gray;
-    display: grid; 
-  grid-template-columns: auto 1fr; 
-  grid-template-rows: 1fr; 
-  gap: 0px 0px; 
-  grid-template-areas: 
-    "col1 col2"; 
-}
 
-.col1 { grid-area: col1;}
-
-.col2 { grid-area: col2; }
 
     </style>
 </head>
 <body>
     
     <div style="clear:both; position:relative;">
-        <div style="position:absolute; left:0pt; width:192pt;">
+        <div style="position:absolute; left:0pt; width:50pt;">
+            <img src="data:image/png;base64, {!! $qrcode !!}"/>
+        </div>
+        <div style="margin-left:50pt;">
+
             <strong>NoPlaca:</strong>
                 {{ $inventory->noplaca }}
         </br>
@@ -35,9 +27,7 @@
          
                 <strong>Tipo:</strong>
                 {{ $inventory->typeproduct->name }}
-        </div>
-        <div style="margin-left:100pt;">
-            <img src="data:image/png;base64, {!! $qrcode !!}"/>
+            
         </div>
     </div>
 
